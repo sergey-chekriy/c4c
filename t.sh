8b9f49b6576cdbe0d974b602e558dc71ab9d4228
sergey-chekriy <sergey.chekriy@me.com>
2026-06-26 19:26:05 +0000

    m8.6fix

--- t.sh
+++
@@ -1,7 +0,0 @@
-cargo fmt
-cargo clippy -- -D warnings
-cargo test
-make grammar-test
-
-cargo run -- validate tests/fixtures/m83-archimate-profile.dsl
-cargo run -- inspect tests/fixtures/m83-archimate-profile.dsl
